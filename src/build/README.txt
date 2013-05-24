884f1c953a6af79ab5eb64334be0976087f2e311
Eric Parker <theriex@gmail.com>
2013-05-23 20:11:39 -0400

    added a build for an optimized compiled load

---
+++ src/build/README.txt
@@ -0,0 +1,21 @@
+
+THE BUILD SYSTEM IS NOT REQUIRED FOR DEVELOPMENT.  It's goal is to
+create a minified combined source file that takes care of all the AMD
+module requirements in one chunk so things load faster.
+
+To run the build, you will need 
+ 1. node.js installed
+ 2. the google closure compiler unpacked into a "compiler-latest"
+    subdirectory off of here
+ 3. a java runtime (needed by the compiler)
+
+To create a minified file with all the source (optimized load) run
+
+node build.js
+
+
+To replace the minified file with an empty file (normal AMD load) run
+
+node build.js clean
+
+
---
+++ src/build/build.js
@@ -0,0 +1,110 @@
+//For info, please see readme.txt
+
+var morbuild = (function () {
+    "use strict";
+
+    var fs = require('fs'),
+        cp = require('child_process'),
+        readopt = { encoding: 'utf8' },
+        writeopt = { encoding: 'utf8' },
+        outsrc = "mor-source.js",
+        outcomp = "../../docroot/js/mor-comp.js",
+
+
+    getModuleFiles = function (nextfunc) {
+        fs.readFile('../../docroot/js/mor.js', readopt, function (err, text) {
+            var modules, i, modfile, fpath, modefs = [];
+            if(err) {
+                throw err; }
+            text = text.slice(text.indexOf("mor.init1 = function"));
+            text = text.slice(text.indexOf("require(mor.cdnconf,"));
+            text = text.slice(text.indexOf("["));
+            text = text.slice(1, text.indexOf("]"));
+            modules = text.split(",");
+            for(i = 0; i < modules.length; i += 1) {
+                modfile = modules[i].trim();
+                modfile = modfile.slice(1, modfile.length - 1);
+                if(modfile.indexOf("amd/") === 0) {
+                    fpath = "../../docroot/js/" + modfile + ".js";
+                    modefs.push({ filepath: fpath, module: modfile }); }
+                else if(modfile.indexOf("ext/") === 0) {
+                    fpath = "../../docroot/js/amd/" + modfile + ".js";
+                    modefs.push({ filepath: fpath, module: modfile }); } }
+            nextfunc(modefs);
+        });
+    },
+
+
+    appendSourceFile = function (modefs, index, nextfunc) {
+        if(index < modefs.length) {
+            fs.readFile(modefs[index].filepath, readopt, function (err, text) {
+                var insidx;
+                if(err) {
+                    throw err; }
+                insidx = text.indexOf("define(") + "define(".length;
+                text = text.slice(0, insidx) + 
+                    "\"" + modefs[index].module + "\", " +
+                    text.slice(insidx);
+                fs.appendFile(outsrc, text, writeopt, function (err) {
+                    if(err) {
+                        throw err; }
+                    appendSourceFile(modefs, index + 1, nextfunc); }); }); }
+        else { //all files appended
+            console.log("Source files appended");
+            nextfunc(); }
+    },
+
+
+    aggregateSource = function (modefs, nextfunc) {
+        fs.writeFile(outsrc, "", writeopt, function (err) {
+            if(err) {
+                throw err; }
+            appendSourceFile(modefs, 0, nextfunc);
+        });
+    },
+
+
+    minifyAndDeploy = function (srcfile) {
+        var command, args;
+        command = "java -jar compiler-latest/compiler.jar --js " + outsrc + 
+            " --js_output_file " + outcomp;
+        console.log(command);
+        args = command.split(" ");
+        cp.spawn(args[0], args.slice(1));
+    },
+
+
+    runbuild = function () {
+        getModuleFiles(function (modefs) {
+            aggregateSource(modefs, function () {
+                minifyAndDeploy(); }); });
+    },
+
+
+    cleanbuild = function () {
+        fs.writeFile(outcomp, "", writeopt, function (err) {
+            if(err) {
+                throw err; }
+            console.log("cleared out " + outcomp); 
+        });
+    };
+
+
+    return {
+        run: function () {
+            runbuild(); },
+        clean: function () {
+            cleanbuild(); }
+    };
+
+} () );
+
+
+if(process.argv[2] === "clean") {
+    morbuild.clean(); }
+else {
+    morbuild.run(); }
+
+
+
+
