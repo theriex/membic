d44cfa4b42db53d99b31ddf35244340818e5ab63
Eric Parker <theriex@gmail.com>
2014-01-12 11:05:01 -0500

    need a client info debug page for support purposes

---
+++ docroot/js/amd/clientcheck.js
@@ -0,0 +1,71 @@
+/*global jt: false, jtminjsDecorateWithUtilities: false */
+/*jslint unparam: true, white: true, maxerr: 50, indent: 4 */
+
+//This is a degenerate standalone module used for testing.  Don't model it.
+var clientcheck = (function () {
+    "use strict";
+
+    ////////////////////////////////////////
+    // closure variables
+    ////////////////////////////////////////
+
+    var 
+
+
+    ////////////////////////////////////////
+    // helper functions
+    ////////////////////////////////////////
+
+    navHTML = function () {
+        var html = "No navigator information available.", nav;
+        if(navigator) {
+            nav = navigator;
+            html = ["table",
+                    [["tr",
+                      [["td", {cla: "attrtd"}, "appCodeName:"],
+                       ["td", {cla: "valtd"}, nav.appCodeName]]],
+                     ["tr",
+                      [["td", {cla: "attrtd"}, "appName:"],
+                       ["td", {cla: "valtd"}, nav.appName]]],
+                     ["tr",
+                      [["td", {cla: "attrtd"}, "appVersion:"],
+                       ["td", {cla: "valtd"}, nav.appVersion]]],
+                     ["tr",
+                      [["td", {cla: "attrtd"}, "platform:"],
+                       ["td", {cla: "valtd"}, nav.platform]]],
+                     ["tr",
+                      [["td", {cla: "attrtd"}, "userAgent:"],
+                       ["td", {cla: "valtd"}, nav.userAgent]]]]]; }
+        return html;
+    },
+
+
+    clientInfoHTML = function () {
+        var html;
+        html = [["p", 
+                 "Welcome to the client test page, here's your info:"],
+                navHTML(),
+                ["p",
+                 "isLowFuncBrowser: " + jt.isLowFuncBrowser()],
+                ["p",
+                 "&nbsp;"],
+                ["p",
+                 "Please copy and paste the entire text of this page " +
+                 "when emailing support"]];
+        return html;
+    };
+
+
+    ////////////////////////////////////////
+    // published functions
+    ////////////////////////////////////////
+return {
+
+    run: function () {
+        jtminjsDecorateWithUtilities(jt);
+        jt.out('outputdiv', jt.tac2html(clientInfoHTML()));
+    }
+
+}; //end of returned functions
+}());
+
