3edf52670e9a6f67be0705b2c2ccc97ec75f6167
Eric Parker <theriex@gmail.com>
2015-07-29 14:41:02 -0400

    Toughened up imagerelay and return a blank img on fail

--- membicsys/src/py/consvc.py
+++ membicsys/src/py/consvc.py
@@ -422,7 +422,12 @@ class ImageRelay(webapp2.RequestHandler):
             logging.info("ImageRelay retrieved from cache")
         else:
             try:
-                result = simple_fetchurl(self, url)
+                result = urlfetch.fetch(url, payload=None, method="GET",
+                                        headers=headers,
+                                        allow_truncated=False, 
+                                        follow_redirects=True, 
+                                        deadline=10, 
+                                        validate_certificate=False)
                 if result:
                     logging.info("ImageRelay urlfetch successful")
                     img = images.Image(result.content)
@@ -430,7 +435,14 @@ class ImageRelay(webapp2.RequestHandler):
                     img = prepare_image(img)
                     memcache.set(url, pickle.dumps(img))
             except Exception as e:
-                logging.info("ImageRelay " + str(e))
+                img = None
+                logging.info("ImageRelay urlfetch exception: " + str(e))
+            if not img:
+                # hex values for a 4x4 transparent PNG created with GIMP:
+                imgstr = "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a\x00\x00\x00\x0d\x49\x48\x44\x52\x00\x00\x00\x04\x00\x00\x00\x04\x08\x06\x00\x00\x00\xa9\xf1\x9e\x7e\x00\x00\x00\x06\x62\x4b\x47\x44\x00\xff\x00\xff\x00\xff\xa0\xbd\xa7\x93\x00\x00\x00\x09\x70\x48\x59\x73\x00\x00\x0b\x13\x00\x00\x0b\x13\x01\x00\x9a\x9c\x18\x00\x00\x00\x07\x74\x49\x4d\x45\x07\xdd\x0c\x02\x11\x32\x1f\x70\x11\x10\x18\x00\x00\x00\x0c\x69\x54\x58\x74\x43\x6f\x6d\x6d\x65\x6e\x74\x00\x00\x00\x00\x00\xbc\xae\xb2\x99\x00\x00\x00\x0c\x49\x44\x41\x54\x08\xd7\x63\x60\xa0\x1c\x00\x00\x00\x44\x00\x01\x06\xc0\x57\xa2\x00\x00\x00\x00\x49\x45\x4e\x44\xae\x42\x60\x82"
+                img = images.Image(imgstr)
+                img.resize(width=4, height=4)
+                img = img.execute_transforms(output_encoding=images.PNG)
         if img:
             self.response.headers['Content-Type'] = "image/png"
             self.response.out.write(img)
