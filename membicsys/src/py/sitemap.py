76d23899efb8f31ff28158c1e0e99524e9180394
Eric Parker <theriex@gmail.com>
2016-11-10 17:51:17 -0500

    Include most recently modified 100 theme urls

--- membicsys/src/py/sitemap.py
+++ membicsys/src/py/sitemap.py
@@ -1,5 +1,9 @@
 import webapp2
 import logging
+from cacheman import *
+from morutil import *
+import coop
+
 
 head = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -51,7 +55,18 @@ foot = """
 
 class SitemapXML(webapp2.RequestHandler):
     def get(self):
-        xml = head + foot
+        xml = head
+        vq = VizQuery(coop.Coop, "ORDER BY modified DESC")
+        ctms = vq.fetch(100, read_policy=db.EVENTUAL_CONSISTENCY, deadline=10)
+        for ctm in ctms:
+            xml += "<url>"
+            if ctm.hashtag:
+                xml += "  <loc>$DOCROOT/" + ctm.hashtag + "</loc>"
+            else:
+                xml += "  <loc>$DOCROOT/t/" + str(ctm.key().id()) + "</loc>"
+            xml += "  <lastmod>" + ctm.modified[0:10] + "</lastmod>"
+            xml += "</url>"
+        xml += foot
         xml = xml.replace("$DOCROOT", self.request.host_url)
         self.response.headers['Content-Type'] = 'text/xml'
         self.response.out.write(xml)
