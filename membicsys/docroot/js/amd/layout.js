1240f4b7aea08bebed583bad757cfc6c8a26a4e9
Eric Parker <theriex@gmail.com>
2017-09-03 15:56:45 -0400

    cachev update (script loading cache bust version string)

--- membicsys/docroot/js/amd/layout.js
+++ membicsys/docroot/js/amd/layout.js
@@ -175,7 +175,7 @@ app.layout = (function () {
         var html = ["a", {href: "#community", title: "Community membics",
                            onclick: jt.fs("app.activity.displayFeed('all')")},
                      ["img", {id: "logoimg", 
-                              src: "img/membiclogo.png?v=170902"}]];
+                              src: "img/membiclogo.png?v=170903"}]];
         jt.out("logodiv", jt.tac2html(html));
         jt.byId("bottomnav").style.display = "none";
     },
@@ -493,12 +493,12 @@ return {
                                "d3script"); }
             if(typeof d3ckit === "undefined") { //mac ff idiom
                 app.loadScript("loadSlideDecks", 
-                               "js/static/d3ckit.js?v=170902",
+                               "js/static/d3ckit.js?v=170903",
                                "d3ckitscript"); }
             decknames.forEach(function (deckname) {
                 modnames.push("js/static/" + deckname); });
             jt.loadAppModules(app, modnames, window.location.href,
-                              app.layout.displaySlideDecks, "?v=170902"); }
+                              app.layout.displaySlideDecks, "?v=170903"); }
     },
 
 
--- membicsys/docroot/js/amd/review.js
+++ membicsys/docroot/js/amd/review.js
@@ -1546,7 +1546,7 @@ app.review = (function () {
                     [["div", {cla: "pcdsectiondiv"},
                       ["a", {href: href},
                        [["img", {cla: "reviewbadge", 
-                                 src: "img/membiclogo.png?v=170902"}],
+                                 src: "img/membiclogo.png?v=170903"}],
                         errtxt]]],
                      ["div", {cla: "dlgbuttonsdiv"},
                       [["button", {type: "button", id: "cancelsieb",
