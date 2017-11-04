301c0863b8599f663af8a10185720f9b5b84ace1
Eric Parker <theriex@gmail.com>
2017-11-04 12:43:07 -0400

    cachev update (script loading cache bust version string)

--- membicsys/docroot/js/amd/review.js
+++ membicsys/docroot/js/amd/review.js
@@ -1559,7 +1559,7 @@ app.review = (function () {
                     [["div", {cla: "pcdsectiondiv"},
                       ["a", {href: href},
                        [["img", {cla: "reviewbadge", 
-                                 src: "img/membiclogo.png?v=171026"}],
+                                 src: "img/membiclogo.png?v=171104"}],
                         errtxt]]],
                      ["div", {cla: "dlgbuttonsdiv"},
                       [["button", {type: "button", id: "cancelsieb",
