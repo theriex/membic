4e13a2bf34850dc25f2cc1743b114c4641b13a00
Eric Parker <theriex@gmail.com>
2019-08-13 11:21:14 -0400

    updated cache bust version stamp

--- membicsys/src/py/start.py
+++ membicsys/src/py/start.py
@@ -13,7 +13,7 @@ from operator import attrgetter, itemgetter
 
 # Provide appropriate source for the main page or hashtag specified page.
 
-cachev = "v=190812"
+cachev = "v=190813"
 
 indexHTML = """
 <!doctype html>
