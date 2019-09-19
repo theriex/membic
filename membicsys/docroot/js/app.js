eb545f07f1493feaeea5b50338eaea997d9ee9c9
Eric Parker <theriex@gmail.com>
2019-09-19 12:22:07 -0400

    updated cache bust version stamp

--- membicsys/docroot/js/app.js
+++ membicsys/docroot/js/app.js
@@ -220,7 +220,7 @@ var jt = {};   //Global access to general utility methods
         jt.out("loadstatusdiv", "Loading app modules...");
         app.amdtimer = {};
         app.amdtimer.load = { start: new Date() };
-        jt.loadAppModules(app, modules, href, app.init2, "?v=190918");
+        jt.loadAppModules(app, modules, href, app.init2, "?v=190919");
     };
 
 
@@ -233,7 +233,7 @@ var jt = {};   //Global access to general utility methods
         if(!href.startsWith("http")) {
             href = jt.baseurl(window.location.href) + "/" + href; }
         if(href.indexOf("v=") < 0) {
-            href += "?v=190918"; }
+            href += "?v=190919"; }
         js = document.createElement("script");
         //js.async = true;
         js.type = "text/javascript";
