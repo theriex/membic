59556072c65068b6d6bba3b1336437508e9b3f66
Eric Parker <theriex@gmail.com>
2020-05-27 19:01:48 +0200

    cachev update

--- membicsys/py/start.py
+++ membicsys/py/start.py
@@ -19,7 +19,7 @@ import py.feed as feed
 # This cache bust value is updated via membic/build/cachev.js which keeps
 # all the cache bust values updated and in sync across the sourcebase.  Do
 # not edit it directly.
-cachev = "v=200526"
+cachev = "v=200527"
 
 indexHTML = """
 <!doctype html>
