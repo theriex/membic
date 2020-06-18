e00660ef53a2d392bc2642617b3be69e0e643ee8
Eric Parker <theriex@gmail.com>
2020-06-18 13:22:14 -0400

    cachev update

--- membicsys/public/js/app.js
+++ membicsys/public/js/app.js
@@ -130,7 +130,7 @@ var jt = {};   //Global access to general utility methods
         jt.out("loadstatusdiv", "Loading app modules...");
         app.amdtimer = {};
         app.amdtimer.load = { start: new Date() };
-        jt.loadAppModules(app, modules, app.docroot, app.init2, "?v=200613");
+        jt.loadAppModules(app, modules, app.docroot, app.init2, "?v=200618");
     };
 
 
